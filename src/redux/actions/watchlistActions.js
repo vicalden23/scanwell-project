210a9583b989cdfd85282e3ddfcae3c4a78b3721
vic.alden23 <vic.alden23@gmail.com>
2018-10-01 17:57:30 -0700

    create actions for watchlist management

---
+++ src/redux/actions/watchlistActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { 
+	ADD_TO_WATCHLIST,
+	FETCH_WATCHLIST_MOVIES,
+} from '../constants';
+
+
+export const getWatchlistMovies = () => (dispatch) => {
+	axios.get('http://localhost:2023/api/movies/watchlist')
+		.then(response => dispatch({
+			type: FETCH_WATCHLIST_MOVIES,
+			payload: response.data,
+		}))
+		.catch((err) => {
+			throw new Error(`Could not fetch watchlist movies: ${err}`)
+		})
+}
+
+export const addToWatchlist = (data) => (dispatch) => {
+	console.log(data)
+	axios.post('http://localhost:2023/api/movies/watchlist', data)
+		.then(response => dispatch({
+			type: ADD_TO_WATCHLIST,
+			payload: response.data,
+		}))
+		.catch(() => {
+			throw new Error(`Could not fetch movies`)
+		})
+}
