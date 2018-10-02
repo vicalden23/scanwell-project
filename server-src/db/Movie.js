b48a7b0f2481f5537bc8be293d4dbc1b94b62dca
vic.alden23 <vic.alden23@gmail.com>
2018-10-01 18:00:27 -0700

    define movie schema

---
+++ server-src/db/Movie.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('../config')
+
+// connect to db
+mongoose.connect(config.db.url, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log(`Mongo connection open on ${config.db.url}`);
+});
+
+// define schema
+const movieSchema = new mongoose.Schema({
+  title: { type: String },
+  movie_id: { type: Number },
+  seen: { type: Boolean },
+  watchlist: { type: Boolean }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
