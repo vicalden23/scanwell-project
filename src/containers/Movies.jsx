f44bbfce636281f9bbb97c85ca899af0a8f01a3f
vic.alden23 <vic.alden23@gmail.com>
2018-10-02 21:42:07 -0700

    create a container for app

---
+++ src/containers/Movies.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+import SearchMovies from '../components/SearchMovies';
+import Watchlist from '../components/Watchlist';
+import SeenList from '../components/SeenList';
+
+class Movies extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Row>
+            <Col md={6} mdPush={6}>
+              <Watchlist />
+            </Col>
+            <Col md={6} mdPull={6}>
+              <SeenList />
+            </Col>
+          </Row>
+        </Grid>
+        <SearchMovies />
+      </div>
+    )
+  }
+}
+
+export default Movies;
