036355fd76f4aa52ce5fba8e94c97e147b341bf7
vic.alden23 <vic.alden23@gmail.com>
2018-10-02 21:48:47 -0700

    combine reducers for movie data retreival

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
 import watchlistReducer from './watchlistReducer';
-import seenReducer from './seenReducer'
+import seenReducer from './seenReducer';
+import movieDetailsReducer from './movieDetailsReducer';
 
 export default combineReducers({
   search: searchReducer,
   watchlist: watchlistReducer,
-  seen: seenReducer
+  seen: seenReducer,
+  details: movieDetailsReducer,
 });
