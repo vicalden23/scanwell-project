c968d3be06eac1101bcc3e20eeab1c21a7fe26d1
vic.alden23 <vic.alden23@gmail.com>
2018-10-02 21:44:26 -0700

    create reducer for movie details

---
+++ src/redux/reducers/movieDetailsReducer.js
@@ -0,0 +1,17 @@
+import { FETCH_MOVIE_DETAILS } from '../constants';
+
+const initialState = {
+	details: {},
+}
+
+export default function (state = initialState, action) {
+	switch (action.type) {
+		case FETCH_MOVIE_DETAILS:
+			return {
+				...state,
+				details: action.payload,
+			};
+		default:
+			return state;
+	}
+}
